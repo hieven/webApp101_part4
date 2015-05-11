d16adbc88e1160bdc52080fa86690c67fd647c19
Even <kiki44552002@gmail.com>
2015-05-11 09:59:26 +0800

    add eventEmitter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var circle = require('./circle');
+var events = require('events').EventEmitter;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {
@@ -13,3 +14,9 @@ console.log('Server running at 3000');
 
 
 console.log(circle.area(50));
+
+var ee = new events();
+ee.items = [1, 2, 3, 4];
+ee.on('haha', function() {
+    console.log('changed');
+})
