62ea8332d40d97bcb27f567ac15e0d8db35f4f46
Even <kiki44552002@gmail.com>
2015-05-11 09:58:52 +0800

    add module circle.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var circle = require('./circle');
 
 http.createServer(function(req, res) {
     res.writeHead(200, {
@@ -9,3 +10,6 @@ http.createServer(function(req, res) {
 }).listen(3000);
 
 console.log('Server running at 3000');
+
+
+console.log(circle.area(50));
