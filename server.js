5c474af71b5477957bdc236e4c9c2e22b09c9c33
Even <kiki44552002@gmail.com>
2015-05-11 10:28:41 +0800

    add router

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var url = require('url');
 
-function start() {
+
+function start(route) {
     function onRequest(req, res) {
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
