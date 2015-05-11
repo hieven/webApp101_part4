936c3706845ec3cc0838379f7b148b86dd3d7eb3
Even <kiki44552002@gmail.com>
2015-05-11 10:09:16 +0800

    refactor createServer into callback func.

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 var http = require('http');
-var circle = require('./circle');
-var events = require('events').EventEmitter;
 
-http.createServer(function(req, res) {
+
+function onRequest(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
+    res.write('Hello World');
+    res.end();
+}
 
-    res.end('Hello World');
-}).listen(3000);
+http.createServer(onRequest).listen(3000);
 
 console.log('Server running at 3000');
-
-
-console.log(circle.area(50));
-
-var ee = new events();
-ee.items = [1, 2, 3, 4];
-ee.on('haha', function() {
-    console.log('changed');
-})
